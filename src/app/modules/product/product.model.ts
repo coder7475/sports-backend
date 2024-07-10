09e9ebe1c30ad9e65d6a43dc50bfd5edfa394700
coder7475 <robiulhossain7475@gmail.com>
2024-07-10 20:57:39 +0600

    feat: create a product Model

--- src/app/modules/product/product.model.ts
+++ src/app/modules/product/product.model.ts
@@ -1,4 +1,39 @@
 import { IProduct } from './product.interface'
-import { Schema } from 'mongoose'
+import { model, Schema } from 'mongoose'
 
-const product = new Schema<IProduct>({})
+const productSchema = new Schema<IProduct>({
+  name: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+})
+
+export const Product = model<IProduct>('product', productSchema)
