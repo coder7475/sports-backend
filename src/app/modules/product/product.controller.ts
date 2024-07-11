aa5f747b3d22bbdebcc7e2cffe6005a18cd3eb7e
coder7475 <robiulhossain7475@gmail.com>
2024-07-11 21:41:08 +0600

    feat: filter products by category, brand, price and rating

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -1,34 +1,41 @@
-import httpStatus from 'http-status'
-import catchAsync from '../../utils/catchAsync'
-import sendResponse from '../../utils/sendResponse'
-import { ProductServices } from './product.service'
-import { IQuery } from './product.interface'
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { ProductServices } from './product.service';
+import { IQuery } from './product.interface';
 
 const getProducts = catchAsync(async (req, res) => {
   // build query
-  const query: Partial<IQuery> = {}
+  const query: Partial<IQuery> = {};
   // search by name
-  const name = req.query.name || ''
+  const name = req.query.name || '';
   if (name) {
-    query.name = { $regex: name, $options: 'i' }
+    query.name = { $regex: name, $options: 'i' };
   }
   // filter by category
-  const category = (req.query.category as string) || ''
-  if (category) query.category = category
+  const category = (req.query.category as string) || '';
+  if (category) query.category = category;
   // filter by brand
-  const brand = (req.query.brand as string) || ''
-  if (brand) query.brand = brand
+  const brand = (req.query.brand as string) || '';
+  if (brand) query.brand = brand;
+  // filter by price
+  const price = (req.query.price as string) || 0;
+  if (price) query.price = Number(price);
+  // filter by rating
+  const rating = (req.query.rating as string) || 0;
+  if (rating) query.price = Number(rating);
+
   // get all products from database
-  const result = await ProductServices.getAllProducts(query)
+  const result = await ProductServices.getAllProducts(query);
   // send response
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'Products retrieved successfully',
     data: result,
-  })
-})
+  });
+});
 
 export const ProductController = {
   getProducts,
-}
+};
